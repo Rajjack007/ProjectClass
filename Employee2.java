d02326bcebebee7174fa198224d121eca3dfa531
Rajasekar@12345 <prajaa88@gmail.com>
2023-02-12 15:52:49 +0530

    commiting the change

---
+++ Employee2.java
@@ -0,0 +1,22 @@
+package org.clt;
+
+public class Employee2 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("Before : "+a);
+		System.out.println("Before : "+b);
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("After : "+a);
+		System.out.println("After : "+b);
+		
+	} 
+	
+}
